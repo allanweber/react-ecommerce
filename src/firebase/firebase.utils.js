31d462e5ce8630ba0a29fbd718136cd8c7910278
Allan Weber <a.cassianoweber@gmail.com>
2020-06-03 20:55:02 +0200

    SignUp

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,12 +14,32 @@ const config = {
 };
 
 firebase.initializeApp(config);
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
 export default firebase;
+export const createUSerProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData,
+        });
+      } catch (error) {
+        console.log("Error creating user -> " + error);
+      }
+    } else {
+      console.log("User Already Exists");
+    }
+    return userRef;
+  };
