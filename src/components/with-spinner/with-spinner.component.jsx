1b4e84f00ab147b43b5e8dc533259e234a5e04b7
Allan Weber <a.cassianoweber@gmail.com>
2020-06-09 14:07:52 +0200

    change spinner to return a const

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { SpinnerOverlay, SpinnerContainer } from "./with-spinner.styles";
 
 //higher-order-component
-const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
-  return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer />
-    </SpinnerOverlay>
-  ) : (
-    <WrappedComponent {...otherProps} />
-  );
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
+
+  return Spinner;
 };
 
-
-export default WithSpinner
+export default WithSpinner;
