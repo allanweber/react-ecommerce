64f9a636f4df2e9b8bd4d2c503962d867bf088ef
Allan Weber <a.cassianoweber@gmail.com>
2020-06-16 19:55:16 +0200

    Mod 20 redux saga (#2)
    Convert Project to Sagas

--- src/App.js
+++ src/App.js
@@ -2,48 +2,21 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
+
 import "./App.css";
 import Header from "./components/header/header.component";
-import {
-  auth,
-  createUSerProfileDocument,
-} from "./firebase/firebase.utils";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { setCurrentUser } from "./redux/user/user.actions";
-import { selectCurrentUser } from "./redux/user/user.selector";
 import CheckoutPage from "./pages/checkout/checkout.component";
-import { selectShopCollections } from "./redux/shop/shop.selector";
+
+import { selectCurrentUser } from "./redux/user/user.selector";
+import { checkUserSession } from "./redux/user/user.actions";
 
 class App extends Component {
-  unsubscribeFromAuth = null;
   componentDidMount() {
-    const { setCurrentUserLocal } = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-      if (userAuth) {
-        const userRef = await createUSerProfileDocument(userAuth);
-        userRef.onSnapshot((snapShot) => {
-          setCurrentUserLocal({
-            id: snapShot.id,
-            ...snapShot.data(),
-          });
-        });
-      } else {
-        setCurrentUserLocal(null);
-      }
-    });
-
-    //const collectionArray = Object.keys(collections).map(key => collections[key]);
-
-    // addCollectionsAndItems(
-    //   "collections",
-    //   collectionArray.map(({ title, items }) => ({ title, items }))
-    // );
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
+    const { checkUserSessionFunc } = this.props;
+    checkUserSessionFunc();
   }
 
   redirectIfSignedIn = () => {
@@ -67,11 +40,10 @@ class App extends Component {
 
 const mapStateProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  collections: selectShopCollections,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  setCurrentUserLocal: (user) => dispatch(setCurrentUser(user)),
+  checkUserSessionFunc: () => dispatch(checkUserSession()),
 });
 
 export default connect(mapStateProps, mapDispatchToProps)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,12 +14,27 @@ const config = {
 };
 
 firebase.initializeApp(config);
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
-
-export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+// Previous version with onAuthStateChanged
+// const provider = new firebase.auth.GoogleAuthProvider();
+// provider.setCustomParameters({ prompt: "select_account" });
+// export const auth = firebase.auth();
+// export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
+export const auth = firebase.auth();
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
 
 export default firebase;
 export const createUSerProfileDocument = async (userAuth, additionalData) => {
